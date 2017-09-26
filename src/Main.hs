7a878bdc5bbe8573d2e7b978875736a26ef84c70
Landon GB <landogbland@gmail.com>
2017-09-26 11:21:02 -0600

    Quick and dirty config parsing for limits of snapshots to keep

--- src/Main.hs
+++ src/Main.hs
@@ -1,25 +1,56 @@
 module Main where
 
-import Data.Monoid ((<>))
-import System.Exit (ExitCode (ExitSuccess, ExitFailure), exitWith)
+import qualified Data.ConfigFile as CP
+import           Data.Either.Utils (forceEither) -- provided by MissingH
+import           Data.Monoid ((<>))
+import           System.Exit (ExitCode (ExitSuccess, ExitFailure), exitWith)
 
-import Snapshot
+data SnapshotLimitConfig = SnapshotLimitConfig
+    { yearly       :: !Int
+    , monthly      :: !Int
+    , weekly       :: !Int
+    , daily        :: !Int
+    , hourly       :: !Int
+    , quaterHourly :: !Int
+    } deriving (Show)
+
 
 printAndExit :: String -> IO ()
 printAndExit errMsg = do
     putStrLn errMsg
     exitWith $ ExitFailure 1
 
+-- TODO get rid of forceEither here
+getConfigParser :: String -> IO CP.ConfigParser
+getConfigParser filepath = forceEither <$> CP.readfile CP.emptyCP filepath
+
+-- TODO get rid of forceEither here as well
+getTimelineConfig :: CP.ConfigParser -> Either CP.CPError SnapshotLimitConfig
+getTimelineConfig cp = do
+    keep_yearly        <- CP.get cp "TIMELINE" "keep_yearly"
+    keep_monthly       <- CP.get cp "TIMELINE" "keep_monthly"
+    keep_weekly        <- CP.get cp "TIMELINE" "keep_weekly"
+    keep_daily         <- CP.get cp "TIMELINE" "keep_daily"
+    keep_hourly        <- CP.get cp "TIMELINE" "keep_hourly"
+    keep_quater_hourly <- CP.get cp "TIMELINE" "keep_quater_hourly"
+    return $  SnapshotLimitConfig keep_yearly keep_monthly keep_weekly
+                                  keep_daily keep_hourly keep_quater_hourly
+
 main :: IO ()
 main = do
+    cp <- getConfigParser "apfs-auto-snapshot.cfg"
+    let snapLimitConfig = forceEither $ getTimelineConfig cp
+    putStrLn $ show snapLimitConfig
+
+{-
+    -- Parse command line options
     snapshots <- listSnapshots
     case snapshots of
         Right s -> putStrLn $ show s
         Left s  -> printAndExit $ "Error: " <> s
-{-
-main = do
     snapshot <- createSnapshot
     case snapshot of
         Right s -> putStrLn $ "Created new snapshot: " <> show s
         Left s  -> printAndExit $ "Error: " <> s
 -}
+
